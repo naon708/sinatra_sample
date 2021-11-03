1fe1bb7cb59fa4a3ac46a7f629f021a8c7620cf8
naon708 <naoya_nis@icloud.com>
2021-11-03 16:45:44 +0900

    Leading ActiveRecord

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'sinatra'
-
 gem 'sinatra-contrib'
 
 gem 'sqlite3'
+gem 'sinatra-activerecord'
--- app.rb
+++ app.rb
@@ -1,23 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord' # 追加
+require './models/user.rb' # 追加
 
+# 2. 簡単なレスポンスを返してください
 get '/' do
   "hello world"
 end
 
+# 3. ちょっと装飾したレスポンスを返してください
 get '/html' do
   "<h1>hello world!</h1>"
 end
 
+# 4. ちゃんとHTMLの形式でレスポンスを返してください
 get '/html-completed' do
   "<heml><head></head><body><h1>hello world!</h1></body></html>"
 end
 
+# 5. erbファイルを使ってレスポンスを返してください
 get '/html-completed-erb' do
   erb :html_completed
 end
 
+# 6. link_toメソッドを自前で作ってみてください
 get '/learn-link-to' do
   erb :learn_link_to
 end
@@ -26,10 +33,12 @@ def link_to(text, url)
   "<a href=#{url}>#{text}</a>"
 end
 
+# 7. レイアウトファイルを作ってください
 get '/learn-layout' do
   erb :learn_layout
 end
 
+# 8. データベースを導入してください
 get '/users' do
   db = SQLite3::Database.new 'sample.db'
   rs = db.execute('select * from users;')
@@ -37,4 +46,36 @@ get '/users' do
     { id: row[0], name: row[1] }
   end
   erb :'users/index'
+end
+
+# 9. ユーザーの新規作成機能を作ってください
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  db = SQLite3::Database.new 'sample.db'
+  sql = 'insert into users(name) values(?);'
+  stmt = db.prepare(sql)
+  stmt.bind_params(params[:name])
+  stmt.execute
+
+  redirect to('/users')
+end
+
+# 10. ActiveRecordを導入してユーザー一覧画面を表示してください
+get '/ar/users' do
+  @users = User.all
+  erb :'ar/users/index'
+end
+
+# 11. ActiveRecordを使ってユーザー登録機能を作ってください
+get '/ar/users/new' do
+  @user = User.new
+  erb :'ar/users/new'
+end
+
+post '/ar/users' do
+  User.create(name: params[:name])
+  redirect to('/ar/users')
 end
